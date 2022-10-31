fe6894673c97e99c2fde01ff4028e3c13a8c650e
pourhabibi <sara.pourhabibi1994@gmail.com>
2022-10-31 21:37:45 +0330

    add p.cs

---
+++ p1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           bool content=true;
+           bool no_content=false;
+            Console.WriteLine("text is{0}",content);
+            Console.WriteLine("text is{0}",no_content);
+        }
+    }
+}
