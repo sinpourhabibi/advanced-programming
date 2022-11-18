caf259f26d393115db2a40985af82d6fe6f0a8d5
sinpourhabibi <117066470+sinpourhabibi@users.noreply.github.com>
2022-11-18 23:25:52 +0330

    Add files via upload

---
+++ proj4-27aban.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj4_27aban
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            Double a,b
+                ;
+            Console.WriteLine("enter your inventory");
+            Double x=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter banks Interest:");
+            Double y=Convert.ToDouble(Console.ReadLine());
+            Double z = 0;
+            for (j = 0; j<=x; j++)
+            {
+                z = calc(x, y);
+                if (z >= (2 * x)) ;
+                Console.WriteLine(z);
+                Console.WriteLine(j);
+            }
+            
+               
+        }
+        public static Double calc(Double f,Double r)
+        {
+            Double a = 0;
+            int i;
+            for (i = 0; i < f; i++)
+            {
+                a = (f + r);
+            }
+            return (a);
+            Console.ReadKey();
+        }
+
+        }
+            
+
+    }
+
